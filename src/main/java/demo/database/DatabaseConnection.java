3616f9810c8537a4a7096588e7fe729b6b6579d9
Temetski33 <teemu.laasio@gmail.com>
2026-04-14 10:56:30 +0300

    Move hard coded password to env variable

--- src/main/java/demo/database/DatabaseConnection.java
+++ src/main/java/demo/database/DatabaseConnection.java
@@ -7,7 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/fuel_calculator_localization";
     private static final String USER = "fueluser";
-    private static final String PASSWORD = "mypassword";
+    // Read password from environment variable
+    private static final String PASSWORD = System.getenv("FUELCALC_DB_PASSWORD");
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
