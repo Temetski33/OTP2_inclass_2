fcad19cac203a9531a0b9941bbf295d19f86550c
Temetski33 <teemu.laasio@gmail.com>
2026-04-21 17:26:24 +0300

    Disable application test

--- src/test/java/demo/FuelApplicationTest.java
+++ src/test/java/demo/FuelApplicationTest.java
@@ -1,9 +1,11 @@
 package demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled("Disabled in CI: requires database")
 class FuelApplicationTest {
 
     @Test
