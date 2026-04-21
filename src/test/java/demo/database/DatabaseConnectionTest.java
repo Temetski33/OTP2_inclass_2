3ef27fffb3c7a600f8f0afacc0f0c4c835382c26
Temetski33 <teemu.laasio@gmail.com>
2026-04-21 16:48:11 +0300

    Disable db test for jenkins

--- src/test/java/demo/database/DatabaseConnectionTest.java
+++ src/test/java/demo/database/DatabaseConnectionTest.java
@@ -1,9 +1,11 @@
 package demo.database;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@Disabled("Disabled in CI because no DB is available")
 public class DatabaseConnectionTest {
     @Test
     void testGetConnectionDoesNotThrow() {
